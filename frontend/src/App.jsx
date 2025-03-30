0d7edae015e7647225adb05aadfb391dd32d6598
Chethan S <155093519+SChethanSharma@users.noreply.github.com>
2025-03-30 15:34:17 +0530

    added comments (#2)
    testing git commands using comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import Footer from './components/Footer'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Verify from './pages/Verify'
-
+//xyz
 // this is a new change
 const App = () => {
   return (
