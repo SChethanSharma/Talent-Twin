0bb024e48e762ac2bf1823750e79a5f1f3dd317b
Chethan S <ChethanSharma29@gmail.com>
2025-03-30 15:29:33 +0530

    my commit 2

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -16,6 +16,7 @@ import upload_icon from './upload_icon.png'
 import stripe_logo from './stripe_logo.png'
 import razorpay_logo from './razorpay_logo.png'
 import doc1 from './doc1.png'
+//test1
 import doc2 from './doc2.png'
 import doc3 from './doc3.png'
 import doc4 from './doc4.png'
